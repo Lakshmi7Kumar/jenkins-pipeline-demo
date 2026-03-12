15886aa7dac4b8953a9949a68ff55951b7f4661c
Lakshmi7Kumar <150280506+Lakshmi7Kumar@users.noreply.github.com>
2026-03-12 18:13:40 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
+
+    public static String message() {
+        return "Hello DevOps Pipeline";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline!");
+        System.out.println(message());
     }
 }
